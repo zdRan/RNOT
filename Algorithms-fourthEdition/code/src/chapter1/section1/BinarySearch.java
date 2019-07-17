81763e9aa644efe4b1bba6cdb3c31056c8ab60e8
zdRan <cm.zdran@gmail.com>
2019-07-17 22:15:21 +0800

    添加二分查找非递归版本

---
+++ Algorithms-fourthEdition/code/src/chapter1/section1/BinarySearch.java
@@ -0,0 +1,53 @@
+package chapter1.section1;
+
+/**
+ * Create by ranzd on 2019/7/17
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class BinarySearch {
+
+    public static int binarySearch(int key, int[] array) {
+        return rankByWhile(key, array, 0, array.length - 1);
+    }
+
+    /**
+     * 二分查找算法，递归版
+     *
+     * @param key
+     * @param arr
+     * @param lo
+     * @param hi
+     * @return
+     */
+    private static int rank(int key, int[] arr, int lo, int hi) {
+        if (lo > hi) {
+            return -1;
+        }
+        int mid = lo + (hi - lo) / 2;
+        if (key < arr[mid]) {
+            return rank(key, arr, lo, mid + 1);
+        }
+        if (key > arr[mid]) {
+            return rank(key, arr, mid, hi);
+        }
+        return mid;
+    }
+
+    private static int rankByWhile(int key, int[] arr, int lo, int hi) {
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key == arr[mid]) {
+                return mid;
+            }
+            if (key < arr[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+}
--- Algorithms-fourthEdition/code/src/chapter1/section1/Main.java
+++ Algorithms-fourthEdition/code/src/chapter1/section1/Main.java
@@ -8,13 +8,13 @@ package chapter1.section1;
 public class Main {
     public static void main(String[] args) {
         //System.out.println(gcd(10, 5));
-        int[] arr = new int[11];
+        int[] arr = new int[10];
 
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 10; i++) {
             arr[i] = i + 1;
         }
 
-        System.out.println(binarySearch(5, arr, 0, 10));
+        System.out.println(BinarySearch.binarySearch(5, arr));
     }
 
     /**
@@ -28,27 +28,4 @@ public class Main {
         return q == 0 ? p : gcd(q, p % q);
     }
 
-    /**
-     * 二分查找算法
-     *
-     * @param key
-     * @param arr
-     * @param lo
-     * @param hi
-     * @return
-     */
-    private static int binarySearch(int key, int[] arr, int lo, int hi) {
-        if (lo > hi) {
-            return -1;
-        }
-        int mid = lo + (hi - lo) / 2;
-        if (key < arr[mid]) {
-            return binarySearch(key, arr, lo, mid + 1);
-        }
-        if (key > arr[mid]) {
-            return binarySearch(key, arr, mid, hi);
-        }
-        return mid;
-
-    }
 }
